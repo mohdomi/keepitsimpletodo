0f99b54275ba886e80ae23135e7c662d27481a00
mohdomi <omairm.s.chouhan@gmail.com>
2025-02-28 00:38:19 +0530

    update 2

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,16 @@ const cors = require("cors");
 const userRouter = require("./authentication/index.js")
 const {getAll , todoInput , completed , deleteAllTodos , deleteTodo , updatingTodo} = require("./controller/todo.controller.js")
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname ,"../frontend/dist" )))
+
+app.get("*" , (req,res)=>{
+    res.sendFile(path.join(__dirname , "../frontend/dist" , "index.html"));
+})
 
 app.use("/auth" , userRouter);
 app.get("/" , getAll)
